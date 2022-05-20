0d9f6188fd2fc02c4e5177e1a49934c72c804f05
eshanHaque <eshan.haque16@gmail.com>
2022-05-21 02:13:00 +0600

    updated

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntjiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
