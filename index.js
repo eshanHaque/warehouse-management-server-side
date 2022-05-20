b04a36bee4adb9565f6ecb469781e8f094788377
eshanHaque <eshan.haque16@gmail.com>
2022-05-21 03:02:16 +0600

    cors fix again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors())
+app.use(cors({origin: "*"}))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntjiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
