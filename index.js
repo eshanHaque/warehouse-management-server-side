da847ffef322b67c6966ab99419bcee2ac9e4ca9
eshanHaque <eshan.haque16@gmail.com>
2022-05-21 13:59:05 +0600

    heroku prblm fixed

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    }
+    app.use(cors(corsConfig))
+    app.options("*", cors(corsConfig))
+    app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+    })
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntjiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -32,7 +43,7 @@ async function run() {
         app.get('/manage/:id', async(req, res) =>{
             const id = req.params.id;
             const query={_id: ObjectId(id)};
-            const quantity =  qty - 1
+            const quantity =  qty - 1;
             res.send(quantity);
         });
     }
